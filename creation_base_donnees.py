a9be12829580f247759df668cb016fa9c8ffef11
manonmgd <manon.maugard@ensae.fr>
2024-12-25 10:14:20 +0000

    ajout de commentaires fusion base de données

--- creation_base_donnees.py
+++ creation_base_donnees.py
@@ -5,7 +5,7 @@ df_top_books_fnac_2023 = pd.read_csv("best_sellers_fnac_2023_cleaned.csv")
 df_babelio_top_23 = pd.read_csv("babelio_top_23.csv")
 df_livraddict_prix_2024 = pd.read_csv("livraddict_prix_2024.csv")
 
-# Normaliser les titres pour ignorer la casse (convertir en minuscules)
+# on normalise les titres pour ignorer la casse (on convertie en minuscules)
 df_top_books_fnac_2023['Titre'] = df_top_books_fnac_2023['Titre'].str.lower()
 df_babelio_top_23['Titre'] = df_babelio_top_23['Titre'].str.lower()
 df_livraddict_prix_2024['Titre'] = df_livraddict_prix_2024['Titre'].str.lower()
@@ -14,14 +14,21 @@ df_livraddict_prix_2024['Titre'] = df_livraddict_prix_2024['Titre'].str.lower()
 merged_df = df_top_books_fnac_2023.merge(df_babelio_top_23, on="Titre", how="outer")
 merged_df = merged_df.merge(df_livraddict_prix_2024, on="Titre", how="outer")
 
-# Créer une colonne unique "Auteur" en priorisant les colonnes existantes
+# On crée une colonne unique "Auteur" en priorisant les colonnes existantes
+"""
+l'idée c'est de créer une colonne unique "Auteur" pour réunir toutes les information d'auteur dans une seule variable
+on donne un ordre de priorité: Auteur, Auteur_x, Auteur_y
+si une colonne contient une valeur manquante (NaN), elle est remplit par la valeur de la colonne suivante.
+La méthode .combine_first() remplit les valeurs manquantes (NaN) de la colonne de gauche (merged_df['Auteur']) 
+avec les valeurs correspondantes de la colonne de droite (merged_df['Auteur_x']).
+"""
 merged_df['Auteur'] = merged_df['Auteur'].combine_first(merged_df['Auteur_x']).combine_first(merged_df['Auteur_y'])
 
-# Supprimer les colonnes inutiles "Auteur_x" et "Auteur_y"
+# On supprime les colonnes inutiles "Auteur_x" et "Auteur_y"
 merged_df.drop(columns=['Auteur_x', 'Auteur_y'], inplace=True)
 
-# Enregistrer le résultat dans un fichier CSV
+# On enregistre le résultat dans un fichier CSV
 merged_df.to_csv("merged_books_data.csv", index=False)
 
-# Message de confirmation
-print("Fusion terminée. Les données combinées avec une seule colonne 'Auteur' et insensibilité à la casse sont enregistrées dans 'merged_books_data.csv'.")
+#message de fin pour vérifier
+Print("fusion terminée")
